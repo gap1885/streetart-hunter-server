f078538b50083d89b491de66233658130c488079
Gabriela Arias <gap1885@gmail.com>
2018-10-11 18:58:58 +0200

    modified routes/users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,42 @@
-var express = require('express')
-var router = express.Router()
+'use strict'
 
-/* GET users listing. */
+const express = require('express')
+const router = express.Router()
+const ObjectId = require('mongoose').Types.ObjectId
+
+const User = require('../models/user')
+
 router.get('/', (req, res, next) => {
-  res.send('respond with a resource')
+  User.find({})
+    .then((results) => {
+      res.json(results)
+    })
+    .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+  const { username, password } = req.body
+  if (!username || !password) {
+    return res.status(422).json({ code: 'unprosessable-entity' })
+  }
+  const user = new User(req.body)
+  user.save()
+    .then(() => {
+      res.status(200).json(user)
+    })
+    .catch(next)
+})
+
+router.delete('/:id', (req, res, next) => {
+  const id = req.params.id
+  if (!id || !ObjectId.isValid(id)) {
+    res.status(404).json({ code: 'not-found' })
+  }
+  User.remove({ _id: id })
+    .then(() => {
+      res.json({ code: 'user deleted' })
+    })
+    .catch(next)
 })
 
 module.exports = router
